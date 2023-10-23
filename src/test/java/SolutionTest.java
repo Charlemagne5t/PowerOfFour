fba0c4b613b90cb4cb92e8980fcc8f38636b3994
alek <wait82985@gmail.com>
2023-10-23 15:10:24 +0600

    problem solved

--- src/test/java/SolutionTest.java
+++ src/test/java/SolutionTest.java
@@ -5,22 +5,23 @@ public class SolutionTest {
     @Test
     public void isPowerOfFourTest1(){
         int n = 16;
-        boolean actual = new Solution().isPowerOfFour(16);
+        boolean actual = new Solution().isPowerOfFour(n);
 
         Assert.assertTrue(actual);
     }
     @Test
     public void isPowerOfFourTest2(){
         int n = 5;
-        boolean actual = new Solution().isPowerOfFour(16);
+        boolean actual = new Solution().isPowerOfFour(n);
 
         Assert.assertFalse(actual);
     }
     @Test
     public void isPowerOfFourTest3(){
         int n = 1;
-        boolean actual = new Solution().isPowerOfFour(16);
+        boolean actual = new Solution().isPowerOfFour(n);
 
         Assert.assertTrue(actual);
     }
+
 }
