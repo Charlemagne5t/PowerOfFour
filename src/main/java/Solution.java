15599dd45e645ef121179d612e384ca907126a05
alek <wait82985@gmail.com>
2025-08-15 15:08:40 +0500

    problem solved

--- src/main/java/Solution.java
+++ src/main/java/Solution.java
@@ -1,23 +1,15 @@
-public class Solution {
+class Solution {
     public boolean isPowerOfFour(int n) {
-        if (n < 1) {
+        if(n < 1 ) {
             return false;
         }
-        int indexCounter = 0;
-        boolean metOne = false;
-        while (n > 0) {
-            int rightmostBit = n & 1;
-            if (indexCounter % 2 == 0 && !metOne && rightmostBit == 1) {
-                metOne = true;
-            } else if (indexCounter % 2 == 0 && metOne && rightmostBit == 1) {
-                return false;
-            } else if (indexCounter % 2 == 1 && rightmostBit == 1) {
-                return false;
-            }
-            n >>= 1;
-            indexCounter++;
+        if(n == 1) {
+            return true;
+        }
+        if((n&(n - 1)) == 0 && (Integer.toBinaryString(n).length() -1) % 2 == 0) {
+            return true;
         }
 
-        return true;
+        return false;
     }
 }
